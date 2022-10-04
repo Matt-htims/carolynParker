328c05c6c86ddab636080cb45d0d7f1e33e1d0b7
Matt Smith <matt.smith3108@gmail.com>
2022-10-04 15:03:55 +0100

    Basic setup with files and dependencies

---
+++ pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+	return (
+		<Html>
+			<Head>{/* Fonts go here */}</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
