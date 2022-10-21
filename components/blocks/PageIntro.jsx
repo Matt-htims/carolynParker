8821a08f8096bc684bc12b71781d3e690bcc7636
Matt Smith <matt.smith3108@gmail.com>
2022-10-21 14:26:14 +0100

    Testimonial basic styling

--- components/blocks/PageIntro.jsx
+++ components/blocks/PageIntro.jsx
@@ -1,9 +1,8 @@
 import CustomImage from '../CustomImage';
 
 export default function PageIntro({ content }) {
-	console.log(content);
 	return (
-		<div className="h-screen">
+		<div className="h-screen ">
 			<CustomImage content={content.image} layout="fill" objectFit="cover" />
 		</div>
 	);
--- components/blocks/Testimonial.jsx
+++ components/blocks/Testimonial.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 export default function Testimonial({ content }) {
+	console.log(content);
 	return (
-		<div>
-			<div className="quotemarks"></div>
-			<div className="text">
-				<p className="quote">hi</p>
-				<p className="author">me</p>
+		<div className="max-w-screen-2xl m-auto px-11 relative">
+			<div className="quotemarks w-[80px] h-[60px] bg-cta absolute -left-14 -top-8"></div>
+			<div className="text w-8/12 space-y-7">
+				<p className="quote text-7xl font-accent">{content.quote}</p>
+				<p className="author text-2xl text-cta">{content.author}</p>
 			</div>
 		</div>
 	);
